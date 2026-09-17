48bde0fc511e251e299aba5ce2e9a51d9bc9480e
Ara Hacopian <ara@hacopian.de>
2026-09-17 07:23:31 +0200

    epubsync 0.1.8

--- Formula/epubsync.rb
+++ Formula/epubsync.rb
@@ -1,11 +1,11 @@
 class Epubsync < Formula
   desc "Manage a KEPUB library and sync it to a Kobo"
   homepage "https://github.com/ahacop/epub-sync"
-  version "0.1.7"
+  version "0.1.8"
   license "GPL-3.0-or-later"
 
   url "https://github.com/ahacop/epub-sync/releases/download/v#{version}/epubsync-#{version}-aarch64-apple-darwin.tar.gz"
-  sha256 "aa987900317fafe15ea11b524ad1cb93a0a61e58f5d35737538090cc45468e51"
+  sha256 "ab804987274ae8f114cf576cee3001e32279f71e82268c518aad560c9b4153de"
 
   depends_on arch: :arm64
   depends_on :macos
